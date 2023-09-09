7f2ff28c8b11cfe79e13d42d327f458c7a5a01f7
itsarunkumar <003arunkumark@gmail.com>
2023-09-09 14:34:09 +0530

    loading animation added

--- app/re/loading.tsx
+++ app/re/loading.tsx
@@ -1,10 +1,19 @@
-// Loading component code here (tsx)
-  import React from 'react';
-  
-  export default function reLoading() {
-    return (
-      <div>
-        <h1>reLoading...</h1>
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="w-full h-screen justify-center items-center flex">
+      <div id="load">
+        <div>G</div>
+        <div>N</div>
+        <div>I</div>
+        <div>D</div>
+        <div>A</div>
+        <div>O</div>
+        <div>L</div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+export default Loading;
